662f592efc0f379713ac624faeb47f885556fdb9
Ali Tourani <a.tourani1991@gmail.com>
2021-08-06 01:15:38 +0430

    fix: minor changes related to webpack

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 const path = require('path');
-const webpack = require("webpack");
 
 module.exports = {
     onDemandEntries: {
         maxInactiveAge: 1000 * 60 * 1000,
         pagesBufferLength: 1000,
-    },    
-    webpack: (config, { isServer }) => {
-        const rules = config.module.rules;
-        return config;
     },
     eslint: {
         ignoreDuringBuilds: true,
