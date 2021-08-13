2d737ec474546c1cddf59555648a347772fdd7b5
Ali Tourani <a.tourani1991@gmail.com>
2021-08-14 00:57:21 +0430

    fix: add a title component

--- components/Content/Stepper/StepPrefrences.tsx
+++ components/Content/Stepper/StepPrefrences.tsx
@@ -1,15 +1,20 @@
 import React, { FC } from "react";
 import { Row, Col } from 'antd';
 import { Image } from 'antd';
+import { Typography } from 'antd';
+
+const { Title } = Typography;
 
 const StepPrefrences: FC = () => {
   return <Row gutter={24}>
     <Col span={16}>
-      That
+      <Title level={3}>Let us know more about you!</Title>
+      <Title level={5}>Please choose your prefrences from the list below:</Title>
     </Col>
     <Col span={8}>
     <Image
       width={400}
+      preview={false}
       src="/tourism.png"
     />
     </Col>
