b9c6986ea5b1edacb7d23a538bef23d86941e3a8
Ali Tourani <a.tourani1991@gmail.com>
2021-08-24 00:59:48 +0430

    feat: user rating image

--- components/Content/Stepper/StepPrefrences.tsx
+++ components/Content/Stepper/StepPrefrences.tsx
@@ -8,19 +8,19 @@ const { Title } = Typography;
 
 const StepPrefrences: FC = () => {
   return <Row gutter={24}>
-    <Col span={16}>
-      <Title level={3}>Let us know more about you!</Title>
-      <Title level={5}>Please choose your prefrences from the list below:</Title>
-      <PreferencesItems />
-    </Col>
-    <Col span={8}>
-      <Image
-        width={400}
-        preview={false}
-        src="/tourism.png"
-      />
-    </Col>
-  </Row>
+      <Col span={16}>
+        <Title level={3}>Let us know more about you!</Title>
+        <Title level={5}>Please choose your prefrences from the list below:</Title>
+        <PreferencesItems />
+      </Col>
+      <Col span={8}>
+        <Image
+          width={400}
+          preview={false}
+          src="/user-rating.png"
+        />
+      </Col>
+    </Row>
   };
   
   export default StepPrefrences;
